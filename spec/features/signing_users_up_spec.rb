fa28bc1740e0cb603811a7197899c4f74b3e5ed3
Daniel Uribe <duribe8@gmail.com>
2016-10-04 20:17:19 -0700

    Fix sign_up test

--- spec/features/signing_users_up_spec.rb
+++ spec/features/signing_users_up_spec.rb
@@ -7,6 +7,7 @@ RSpec.feature "Users signup" do
     click_link "Sign up"
     fill_in "Email", with: "user@example.com"
     fill_in "Password", with: "password"
+    fill_in "Password confirmation", with: "password"
     click_button "Sign up"
 
     expect(page).to have_content("You have signed up successfully.")
